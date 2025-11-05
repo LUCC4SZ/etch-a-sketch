2ce5d84c7959887e5512b01b04a85f005aeada48
lucca-dev <luccazamora@gmail.com>
2025-11-05 16:42:02 -0300

    Fix row's height

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const container = document.querySelector("div");
 for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
     row.style.display = "flex";
+    row.style.height = "16%";
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.style.width = "16%";
-        square.style.height = "38.78px";
         square.addEventListener("mouseenter", () => {
             square.classList.add("green-background");
         });

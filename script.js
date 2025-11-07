225f66e0b3b88a21cf5bb8e08f17530f20470c8a
lucca-dev <luccazamora@gmail.com>
2025-11-07 19:13:54 -0300

    Build logic to create grid according to user input

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const body = document.querySelector("body");
+
 const container = document.querySelector("div");
 
-for (let i = 0; i < 16; i++) {
+function createGrid(sqr) {
+    const perc = (sqr * 600) / 100;
+    for (let i = 0; i < sqr; i++) {
     const row = document.createElement("div");
     row.style.display = "flex";
-    row.style.height = "16%";
-    for (let j = 0; j < 16; j++) {
+    row.style.height = `${perc}%`;
+    for (let j = 0; j < sqr; j++) {
         const square = document.createElement("div");
-        square.style.width = "16%";
+        square.style.width = `${perc}%`;
         square.addEventListener("mouseenter", () => {
             square.classList.add("green-background");
         });
@@ -17,3 +21,34 @@ for (let i = 0; i < 16; i++) {
     }
     container.appendChild(row);
 };
+}
+
+// for (let i = 0; i < 16; i++) {
+//     const row = document.createElement("div");
+//     row.style.display = "flex";
+//     row.style.height = "16%";
+//     for (let j = 0; j < 16; j++) {
+//         const square = document.createElement("div");
+//         square.style.width = "16%";
+//         square.addEventListener("mouseenter", () => {
+//             square.classList.add("green-background");
+//         });
+//         square.addEventListener("mouseleave", () => {
+//             square.classList.remove("green-background");
+//         });
+//         row.appendChild(square);
+//     }
+//     container.appendChild(row);
+// };
+
+const btn = document.createElement("button");
+
+btn.innerText = "Click me!";
+
+btn.addEventListener("click", () => {
+    
+    const sqr = window.prompt("How many squares do you want per side?");
+    createGrid(sqr);
+});
+
+body.appendChild(btn);

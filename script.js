72c9c4f88a01b05b1480bfbf2947d53f45b05eaa
lucca-dev <luccazamora@gmail.com>
2025-11-18 17:05:10 -0300

    Fix height and width of squares

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const body = document.querySelector("body");
 const container = document.querySelector("div");
 
 function createGrid(sqr) {
-    const perc = (sqr * 600) / 100;
+    const perc = 600 / sqr;
     for (let i = 0; i < sqr; i++) {
     const row = document.createElement("div");
     row.style.display = "flex";
@@ -23,30 +23,11 @@ function createGrid(sqr) {
 };
 }
 
-// for (let i = 0; i < 16; i++) {
-//     const row = document.createElement("div");
-//     row.style.display = "flex";
-//     row.style.height = "16%";
-//     for (let j = 0; j < 16; j++) {
-//         const square = document.createElement("div");
-//         square.style.width = "16%";
-//         square.addEventListener("mouseenter", () => {
-//             square.classList.add("green-background");
-//         });
-//         square.addEventListener("mouseleave", () => {
-//             square.classList.remove("green-background");
-//         });
-//         row.appendChild(square);
-//     }
-//     container.appendChild(row);
-// };
-
 const btn = document.createElement("button");
 
 btn.innerText = "Click me!";
 
 btn.addEventListener("click", () => {
-    
     const sqr = window.prompt("How many squares do you want per side?");
     createGrid(sqr);
 });
